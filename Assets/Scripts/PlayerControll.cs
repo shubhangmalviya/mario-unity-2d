f2420991eba1b5fa2a11404b71ac0078e3a0c18a
Shubhang Malviya <shubhang.malviya@daffodilsw.com>
2020-06-07 22:15:57 +0530

    Player Run Animation made Ramp in Tile Pallete made Sample Level created

---
+++ Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxSpeed;
+    private Rigidbody2D myRB;
+    private bool facingRight = false;
+    private SpriteRenderer myRenderer;
+    private Animator myAnimator;
+    
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myRenderer = GetComponent<SpriteRenderer>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+        
+        myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
+        myAnimator.SetFloat("MoveSpeed", Mathf.Abs(move));
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        myRenderer.flipX = !myRenderer.flipX;
+    }
+}
